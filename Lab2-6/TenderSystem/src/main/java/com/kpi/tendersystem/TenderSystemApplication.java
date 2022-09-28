9983c5629167e070f823bde4cba9a7bfd8feb5ee
Singachpuck <singachpucktv@gmail.com>
2022-09-28 21:02:55 +0300

    Add Lab2-6 template

---
+++ Lab2-6/TenderSystem/src/main/java/com/kpi/tendersystem/TenderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kpi.tendersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenderSystemApplication.class, args);
+	}
+
+}
---
+++ Lab2-6/TenderSystem/src/test/java/com/kpi/tendersystem/TenderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpi.tendersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
