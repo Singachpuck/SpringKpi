2806c435e57e2231c0d946b9c00d93251e09cc94
TheVitik <lokplay2003@gmail.com>
2022-10-03 23:33:07 +0300

    Search and URL generation

---
+++ Lab2-6/TenderSystem/src/main/java/com/kpi/tendersystem/service/search/impl/KeywordSearch.java
@@ -0,0 +1,35 @@
+package com.kpi.tendersystem.service.search.impl;
+
+import com.kpi.tendersystem.model.Tender;
+import com.kpi.tendersystem.service.search.TenderSearch;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Service
+@Scope("prototype")
+public class KeywordSearch implements TenderSearch {
+    @Override
+    public Collection<Tender> search(Collection<Tender> tenders, String query) {
+        // "hello,world;ok;lets,go"
+        ArrayList<Tender> result = new ArrayList<>();
+
+        for (String subQuery : query.split(";")) {
+            String[] keyWords = subQuery.split(",");
+            result.addAll(tenders.stream().filter(tender -> {
+                boolean isValid = true;
+                for (String keyWord : keyWords) {
+                    if (!tender.getTitle().contains(keyWord)) {
+                        isValid = false;
+                        break;
+                    }
+                }
+                return isValid;
+            }).toList());
+        }
+
+        return result;
+    }
+}
